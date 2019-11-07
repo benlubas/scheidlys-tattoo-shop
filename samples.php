c2e759fd7e2476631013784665c741eaa699d448
Ben Lubas <benmlubas@gmail.com>
2019-11-07 16:41:25 -0500

    php is now a thing

---
+++ samples.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+  <title>Sheidley Tattoo</title>
+  <link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+  <?php 
+    include_once('includes/nav.php');
+  ?>
+
+  <h1>Samples</h1>
+</body>
+
+</html>
