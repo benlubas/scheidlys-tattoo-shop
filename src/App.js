62c5d9f9ac89ac21038c3b9affcb5f467949016e
benlubas <benmlubas@gmail.com>
2019-11-07 19:50:58 -0500

    QuoteCard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import QuoteCard from './components/QuoteCard.js'; 
 
 function App() {
   return (
-    <div>React is just <span style={{fontWieght:'bold', fontSize:'1.2em'}}>better</span></div>
+    <div>
+      <div>React is just <span style={{ fontWieght: 'bold', fontSize: '1.2em' }}>better</span></div>
+      <QuoteCard title="the reason behind all of this" desc="hi" quote="I don't know, go to Scheidlyshandwritingforthepurposeofatattoo.com or something" date="Thur Nov 7, 2019" />
+    </div>
   );
 }
 
---
+++ src/components/QuoteCard.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Typography, Paper } from '@material-ui/core/';
+
+String.prototype.titleCase = function(){
+  return this.split(" ").map(value => value.charAt(0).toUpperCase() + value.substr(1)).join(" "); 
+}
+
+class QuoteCard extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      hovered: false, 
+      likes: 0, 
+      showContext: false, 
+    } 
+    this.styles = {
+      padding: '20px', 
+      
+    }
+  }
+  
+  render() {
+    return (
+      <Paper style={this.styles} square={true} elevation={3}>
+        <Typography variant="h5">
+          {this.props.title.titleCase()}
+        </Typography>
+        <Typography variant="subtitle1">{this.props.desc}</Typography>
+        <Typography variant="body1">"{this.props.quote}"</Typography>
+        <Typography variant="caption">{this.props.date}</Typography>
+      </Paper>
+    );
+  }
+}
+
+export default QuoteCard;
